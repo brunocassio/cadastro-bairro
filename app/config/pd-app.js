43f1a70e7e118710adefadd7c43cc5c7a35552aa
brunocassio <bru9isk@gmail.com>
2016-11-22 13:03:48 -0200

    first commit

---
+++ app/config/pd-app.js
@@ -0,0 +1,10 @@
+angular.module('pdApp',
+    [
+        'ngMessages',
+        'ngAnimate',
+        'toastr',
+        'ui.grid',
+        'ngMaterial',
+        'ui.router',
+        'oc.lazyLoad'
+    ]);
